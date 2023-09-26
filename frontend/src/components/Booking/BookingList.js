7ab56251425f45cf4c9013525cd47447d552377d
sarfraz-kamboh <132363555+sarfraz-kamboh@users.noreply.github.com>
2023-09-26 14:54:25 +0500

    Working on Authorization.

--- frontend/src/components/Booking/BookingList.js
+++ frontend/src/components/Booking/BookingList.js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import BookingContext from "../../context/bookings/BookingContext";
 
 const BookingList = () => {
